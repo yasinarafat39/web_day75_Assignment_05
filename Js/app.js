5a43222d56b85abfc4138f7596f83c61b3442080
Yasin Arafat <yasinarafat2557@gmail.com>
2024-09-24 00:16:09 +0600

    invalid input handled

--- Js/app.js
+++ Js/app.js
@@ -9,34 +9,40 @@ const donationSection = document.getElementById('donationSection');
 
 for (let singleButton of buttons) { 
 singleButton.addEventListener('click', function(){ 
+ 
 
     const donatedAmountField = singleButton.parentNode.parentNode.children[0].children[1];
     const donatedAmount = parseFloat(donatedAmountField.innerText);
     const tryToDonateAmountField = singleButton.parentNode.parentNode.children[3];
     const tryToDonateAmount = parseFloat(tryToDonateAmountField.value);
-    
-    // increase donated Amount and set
-    const newDonatedAmount = donatedAmount + tryToDonateAmount;
-    donatedAmountField.innerText = newDonatedAmount;
-    tryToDonateAmountField.value = '';
-    
-    // substruct from main balance 
     const mainBalance = parseFloat(mainBalanceField.innerText);
-    const newMainBalance = mainBalance - tryToDonateAmount;
-    mainBalanceField.innerText = newMainBalance; 
+
+    if(!tryToDonateAmount || typeof tryToDonateAmount !== 'number' || tryToDonateAmount > mainBalance || tryToDonateAmount < 1){
+        return alert('Invalid Donation amount.')
+    } else{
+        // increase donated Amount and set
+        const newDonatedAmount = donatedAmount + tryToDonateAmount;
+        donatedAmountField.innerText = newDonatedAmount;
+        tryToDonateAmountField.value = '';
+
+        // substruct from main balance  
+        const newMainBalance = mainBalance - tryToDonateAmount;
+        mainBalanceField.innerText = newMainBalance; 
+
+        // add this transaction into the history
+        const cardTitle = singleButton.parentNode.parentNode.children[1].innerText; 
+
+        historySection.innerHTML += `
+            <div class="border-2 p-6 space-y-4 rounded-xl">
+              <h1 class="text-2xl">${tryToDonateAmount} taka is Donated for ${cardTitle}</h1>
+              <p class="text-xl text-gray-600">Date: ${getCurrentDateAndTime()}</p>
+            </div>
+        `
+        // Success Modal
+        // <!-- Open the modal using ID.showModal() method -->
+        my_modal_1.showModal()
+    }
     
-    // add this transaction into the history
-    const cardTitle = singleButton.parentNode.parentNode.children[1].innerText; 
-      
-    historySection.innerHTML += `
-        <div class="border-2 p-6 space-y-4 rounded-xl">
-          <h1 class="text-2xl">${tryToDonateAmount} taka is Donated for ${cardTitle}</h1>
-          <p class="text-xl text-gray-600">Date: ${getCurrentDateAndTime()}</p>
-        </div>
-    `
-    // Success Modal
-    // <!-- Open the modal using ID.showModal() method -->
-    my_modal_1.showModal()
 })
 }
 
